d0ea396d1f29bf34dd781a21c590759b2f0dbe03
Oskar <oskar_ozkizz_wow@hotmail.com>
2016-07-26 16:06:15 +0200

    Added NWvar for crate entity + added functinality
    Added NwVar for the crate entity (To gain acces to entity data from both
    client and server).
    Also added ability to combine two resrouce crates if they have the same
    resource.
    Also added if entity have ENT:shouldDraw = true and

--- gamemode/libs/sv_utility.lua
+++ gamemode/libs/sv_utility.lua
@@ -1,8 +1,11 @@
 entity = entity or {}
 
-function entity.create(ent, pos)
+function entity.create(ent, pos, model)
 local entity = ents.Create( ent )
 if ( !IsValid( entity ) ) then return end
+if (model) then
+entity:SetModel(model)
+end
 entity:SetPos( pos )
 entity:Spawn()
 end
